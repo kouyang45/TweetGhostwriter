4994b860aba8790c5323923442792212db6612ab
kou yang <kyang45@mail.csuchico.edu>
2017-04-06 15:30:48 -0700

    changes and function
    only change in index is the header (h1) into my name. next is to make the funciton writeTweet to have six varibles named (intro, body, end, randIndex, and sentence). for intro place 4 quotes, body 5, and end 2. then in var randIndex make it randomly pick from intro subtracting one because the subtract one is so it won't have a unidentified string because 0 is included. do the same process for body and end. after that make sentence equal to the intro[randIndex];. but for body and end it is writen as (sentence +" "+ body[randIndex];. finally to update it you will have to use document.getElementById("tweet").innerHTML=sentence.

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,8 +1,16 @@
 function writeTweet()
 {
-  
+  var intro = ["No act of kindness is ever to small and wasted,", "When we seek to discover the best in other, we somehow bring out the best in ourselves,", "A champion is someone who gets up when he can't,", "Shoot for the moon and if you miss you will still be among the stars,"];
+  var body = ["with this weather,", "in the first sight of snowfall,", "beside the clam sound of waterflow,", "to hear music speak to us,", "to rise as the morning sunrise,"]
+  var end = ["for a good day.", "for a good life."]
+  var randIndex = randomUpTo(intro.length-1);
+  var sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length-1)
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(end.length-1)
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML=sentence
 }
-
 //Generates a random whole number between 0 and max (inclusive)
 function randomUpTo(max)
 {
